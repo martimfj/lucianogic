c193646959743d768ef0c2f5f63c9b7940631070
Martim <martim.ferreira.jose@gmail.com>
2017-04-03 19:00:24 -0300

    Checkbox plotando pela janela, imagem dando erro.

--- src/main/java/br/pro/hashi/ensino/desagil/lucianogic/view/GatesView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/lucianogic/view/GatesView.java
@@ -1,12 +1,22 @@
 package br.pro.hashi.ensino.desagil.lucianogic.view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
 
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import br.pro.hashi.ensino.desagil.lucianogic.model.Gate;
 import br.pro.hashi.ensino.desagil.lucianogic.model.Switch;
@@ -15,11 +25,12 @@ import br.pro.hashi.ensino.desagil.lucianogic.model.Switch;
 
 // Esta classe representa a interface de uma calculadora de densidade, com
 // os dois campos de entrada (peso e raio) e o campo de saida (resultado).
-public class GatesView extends JPanel implements ItemListener {
+public class GatesView extends FixedPanel implements ItemListener {
 
 	// Necessario para serializar objetos desta classe.
 	private static final long serialVersionUID = 1L;
 
+	private Image image;
 
 	// A componente JTextField representa um campo para digitacao de texto.
 	// https://docs.oracle.com/javase/tutorial/uiswing/components/textfield.html
@@ -35,8 +46,11 @@ public class GatesView extends JPanel implements ItemListener {
 	private Gate gate;
 
 	public GatesView(Gate gate) {
+		super(600, 360);
 		this.gate = gate;
 		int size = gate.getSize();
+		
+		//image = loadImage(gate.toString());
 		
 		buttonA = new Switch();
 		buttonB = new Switch();
@@ -52,13 +66,6 @@ public class GatesView extends JPanel implements ItemListener {
 			gate.connect(buttonC, 2);
 		}
 		
-		
-		// A componente JLabel representa simplesmente um texto fixo.
-		// https://docs.oracle.com/javase/tutorial/uiswing/components/label.html
-		JLabel inputLabel = new JLabel("Entradas:");
-		JLabel outputLabel = new JLabel("Saída:");
-		
-		add(inputLabel);
 		inputBoxA = new JCheckBox("A");
 		inputBoxB = new JCheckBox("B");
 		inputBoxC = new JCheckBox("Selector");
@@ -70,32 +77,24 @@ public class GatesView extends JPanel implements ItemListener {
 		outputBox = new JCheckBox("Saída");
 		outputBox.setEnabled(false);
 
-
-		// Esta linha garante que os componentes sejam simplesmente
-		// colocados em linha reta, mais especificamente na vertical.
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-		/* A PARTIR DESTE PONTO VOCE DEVE ENTENDER SOZINHO */
-
-		add(inputBoxA);
-		add(inputBoxB);
+		add(inputBoxA,50,50, 30, 30);
+		add(inputBoxB,50,30, 30, 30);
 		
 		if(size == 2 || size == 3){
-			add(inputBoxB);
+			add(inputBoxB,50,30, 30, 30);
 		}
 		else {
 			remove(inputBoxB);
 		}
 		
 		if(size == 3){
-			add(inputBoxC);
+			add(inputBoxC,50,70, 30, 30);
 		}
 		else {
 			remove(inputBoxC);
 		}
 		
-		add(outputLabel);
-		add(outputBox);
+		add(outputBox,50,100, 30, 30);
 		
 		if(gate.read() == false){
 			outputBox.setSelected(false);
@@ -130,5 +129,19 @@ public class GatesView extends JPanel implements ItemListener {
 			outputBox.setSelected(true);
 		}
 	}
+	
+	// Necessario para carregar os arquivos de imagem.
+	private Image loadImage(String filename) {
+		URL url = getClass().getResource("/img/" + filename + ".png");
+		ImageIcon icon = new ImageIcon(url);
+		return icon.getImage();
+	}	
 
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(image, 0, 0, 600, 360, null);
+
+		// Evita bugs visuais em alguns sistemas operacionais.
+		getToolkit().sync();
+	}
 }
